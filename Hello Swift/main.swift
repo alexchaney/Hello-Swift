9d4d3055a7db96ae5d00a5c2d59d26a67d6b5be2
Alex Chaney <Alex@AlexChaney.com>
2020-10-29 23:49:24 -0500

    Initial Commit

---
+++ Hello Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hello Swift
+//
+//  Created by Alexander Chaney on 10/29/20.
+//  Copyright © 2020 UrbanAlex. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
